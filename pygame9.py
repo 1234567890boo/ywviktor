79cdd7f46be87e99f6181ab68824c4b4703375b5
1234567890boo <viktor.meydbray19@mydiscoveryk8.org>
2016-10-08 18:19:29 -0700

    weee

--- pygame9.py
+++ pygame9.py
@@ -3,7 +3,7 @@ import random
 import pygame
 from pygame.locals import *
 pygame.init()
-screen=pygame.display.set_mode((300,300))
+screen=pygame.display.set_mode((300,400))
 pygame.display.set_caption("tictactoe")
 red=(255,0,0)
 green=(0,255,0)
@@ -11,42 +11,64 @@ blue=(0,0,255)
 white=(255,255,255)
 black=(0,0,0)
 chance=1
-boo={1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:'',9:''}
+boo={'1':'','2':'','3':'','4':'','5':'','6':'','7':'','8':'','9':''}
+boo2={(0,0):'1',(100,0):'2',(200,0):'3',(0,100):'4',(100,100):'5',(200,100):'6',(0,200):'7',(100,200):'8',(200,200):'9'}
 v="player1 win's"
 b="player2 win's"
 def WIN():
+    pygame.display.update()
     if boo['1']==boo['2']==boo['3']=='x':
         print(v)
+        exit()
     if boo['4']==boo['5']==boo['6']=='x':
         print(v)
+        exit()
     if boo['7']==boo['8']==boo['9']=='x':
         print(v)
+        exit()
     if boo['3']==boo['6']==boo['9']=='x':
         print(v)
+        exit()
     if boo['2']==boo['5']==boo['8']=='x':
         print(v)
+        exit()
     if boo['1']==boo['4']==boo['7']=='x':
         print(v)
+        exit()
     if boo['3']==boo['5']==boo['7']=='x':
         print(v)
+        exit()
     if boo['1']==boo['4']==boo['9']=='x':
         print(v)
     if boo['1']==boo['2']==boo['3']=='o':
         print(b)
+        exit()
     if boo['4']==boo['5']==boo['6']=='o':
         print(b)
+        exit()
     if boo['7']==boo['8']==boo['9']=='o':
         print(b)
+        exit()
     if boo['3']==boo['6']==boo['9']=='o':
         print(b)
+        exit()
     if boo['2']==boo['5']==boo['8']=='o':
         print(b)
+        exit()
     if boo['1']==boo['4']==boo['7']=='o':
         print(b)
+        exit()
     if boo['3']==boo['5']==boo['7']=='o':
         print(b)
+        exit()
     if boo['1']==boo['4']==boo['9']=='o':
         print(b)
+        exit()
+        def show_text(msg,x,y,color):
+
+            fontobj= pygame.font.SysFont("sansscript",32)
+            msgobj = fontobj.render(msg,False,color)
+            screen.blit(msgobj,(x,y))
 def box_x(x,y):
     pygame.draw.line(screen,white,(x,y),(x+100,y+100),5)
     pygame.draw.line(screen,white,(x+100,y),(x,y+100),5)
@@ -56,21 +78,27 @@ pygame.draw.line(screen,white,(100,0),(100,300),5)
 pygame.draw.line(screen,white,(200,0),(200,300),5)
 pygame.draw.line(screen,white,(0,100),(300,100),5)
 pygame.draw.line(screen,white,(0,200),(300,200),5)
+pygame.draw.line(screen.white,(0,300),(300,300),5)
 while True:
+   pygame.display.update()
    for event in pygame.event.get():
         if event.type==QUIT:
             pygame.quit()
             exit()
         elif event.type==pygame.MOUSEBUTTONDOWN and event.button==1:
-            print('mouse at:',event.pos)
             x=event.pos[0]-event.pos[0]%100
             y=event.pos[1]-event.pos[1]%100
             number=int(x/100)+int(y/100)*3+1
-            if chance%2==1:
-                box_o(x,y)
-            else:
-                box_x(x,y)
-            chance=chance+1
-            WIN()
-        pygame.display.update()
+            box_num=(boo2[x,y])
+            if boo[box_num]=='':
+
+                if chance%2==1:
+                    box_o(x,y)
+                    boo[box_num]='o'
+                else:
+                    box_x(x,y)
+                    boo[box_num]='x'
+                chance=chance+1
+                WIN()
+                    
         
