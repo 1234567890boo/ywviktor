fbe9d09a8f1061e364ccbd2529f0672dbc319a5a
viktor <viktor.meydbray19@mydiscoveryk8.org>
2016-10-09 19:06:15 +0000

    weee

--- pong.py
+++ pong.py
@@ -15,6 +15,22 @@ LP_U=0
 LP_D=0
 RP_U=0
 RP_D=0
+BXM=320
+BYM=240
+RX=random.randint(-2,2)
+RY=random.randint(-2,2)
+def BBB(RR,BXM):
+    RA=random.randint(-2,2)
+    if RA<0 and RR<0:
+        RA=-RA
+        RR=RA
+    if RA>0 and RR>0:
+        RA=-RA
+        RR=RA
+    return RA
+while RX==0:
+    RX=random.randint(-5,5)
+    RY=random.randint(-5,5)
 while True:
     pygame.display.update()
     screen.fill(white)
@@ -26,34 +42,26 @@ while True:
             pygame.quit()
             exit()
         if event.type==KEYDOWN:
-            if event.key==K_w:
-                print('W key pressed')               
+            if event.key==K_w:          
                 LP_U=1
-            elif event.key==K_s:
-                print('S key pressed')               
+            elif event.key==K_s:               
                 LP_D=1
             if event.key==K_UP:
-                print('UP key pressed')
                 RP_U=1
             elif event.key==K_DOWN:
-                print('DOWN key pressed')
                 RP_D=1               
         elif event.type==KEYUP:
             if event.key==K_w:
-                print('W key pressed')
                 LP_U=0
             elif event.key==K_s:
-                print('S key pressed')
                 LP_D=0
             if event.key==K_UP:
-                print('UP key pressed')
                 RP_U=0
             elif event.key==K_DOWN:
-                print('DOWN key pressed')
                 RP_D=0                
-    pygame.draw.rect(screen,blue,(10,LPY,10,150))
-    pygame.draw.rect(screen,green,(590,RPY,10,150))
-    pygame.draw.circle(screen,black,(320,240),10)
+    pygame.draw.rect(screen,blue,(10,LPY,10,100))
+    pygame.draw.rect(screen,green,(610,RPY,10,100))
+    pygame.draw.circle(screen,red,(BXM,BYM),10)
     if LP_U==1:
         LPY=LPY-5
     if LP_D==1:
@@ -61,6 +69,14 @@ while True:
     if RP_U==1:
         RPY=RPY-5
     if RP_D==1:
-        RPY=RPY+5        
-    
-    
+        RPY=RPY+5
+    BXM=BXM+RX
+    BYM=BYM+RY
+    if BXM==640:
+        RX =-RX
+    if BYM==480:
+        RY =-RY
+    if BXM==0:
+        RX =-RX
+    if BYM==0:
+        RY =-RY 
