f8989308c352687ddb2377c40a9cb29cc2ae66cf
Your Name <you@example.com>
2017-06-25 12:10:10 -0700

    weee

--- flappybird.py
+++ flappybird.py
@@ -12,10 +12,18 @@ white=(255,255,255)
 black=(0,0,0)
 x=300
 y=300
+x1=600
+y1=0
+y2=450
 ychange=0
+points=0
+def show_text(msg,x,y,color):
+  fontobj=pygame.font.SysFont("freesans",32)
+  msgobj=fontobj.render(msg,False,color)
+  screen.blit(msgobj,(x,y))
 while True:
   screen.fill(white)
-  pygame.draw.circle(screen,blue,(x,y,),15)
+  pygame.draw.circle(screen,blue,(x,y,),20)
   y=y+ychange 
   for event in pygame.event.get():
     if event.type==QUIT:
@@ -23,10 +31,30 @@ while True:
       exit() 
     if event.type==KEYDOWN:
       if event.key==K_SPACE:
-        ychange=-9
-        print('space key pressed')
+        ychange=-1
     if event.type==KEYUP:
       if event.key==K_SPACE:
         ychange=1
-        print('space key relesed')
+  if y==600:
+    quit()
+  if y==0:
+    quit() 
+  pygame.draw.rect(screen,red,(x1,0,50,y1))
+  pygame.draw.rect(screen,red,(x1,y1+150,50,600))
+  x1=x1-1
+  if x1==0:
+    x1=600
+    x1=x1-1
+    y1=random.randint(50,450)
+  if x1==x and 0<y<y1:
+    quit()
+  if x1==x and y1+150<y<y1+600:
+    quit()
+  if x==x1:
+    points=points+1
+  show_text('points='+str(points),450,10,blue)
+  if points==21:
+     screen.fill(black)
+     show_text('You Win!',300,300,green) 
+     quit()
   pygame.display.update()
