be47a359647d2f677d33800cee3261054067ad21
suraj Rajan <surajrajan0109@gmail.com>
2017-08-27 18:54:19 +0000

    weee

--- animation.py
+++ animation.py
@@ -14,7 +14,8 @@ screen.fill(black)
 x=10
 y=10
 a=0
-runcatr=['runcatr1.png']
+change=0
+runcatr=['runcatr1.png','runcatr2.png','runcatr3.png','runcatr4.png','runcatr5.png','runcatr6.png','runcatr7.png','runcatr8.png']
 runcat=['runcat1.png','runcat2.png','runcat3.png','runcat4.png','runcat5.png','runcat6.png','runcat7.png','runcat8.png',]
 while True:
 ##    for var in runcat:
@@ -24,21 +25,38 @@ while True:
             exit()
         if event.type==KEYDOWN:
             if event.key==K_d:
-                screen.fill(black)
-                x=x+10
-                img=pygame.image.load(runcat[a])
-                a=a+1
-                if a==7:
-                    a=0
-                screen.blit(img,(x,y))
-                if x==1000:
-                    x=0
+                change=1        
+        if event.type==KEYUP:
+            if event.key==K_d:
+                change=0
+        if event.type==KEYDOWN:
             if event.key==K_a:
-                screen.fill(black)
-                x=x-10
-                img=pygame.image.load(runcatr[0])
-                screen.blit(img,(x,y))
-##                    a=a+1
-##                    if a==0:
-##                        a=0
-        pygame.display.update()
+                change=2
+        if event.type==KEYUP:
+            if event.key==K_a:
+                change=0
+
+    if change==1:
+        screen.fill(black)
+        x=x+10
+        img=pygame.image.load(runcat[a])
+        time.sleep(0.05)
+        a=a+1
+        if a==8:
+            a=0
+        screen.blit(img,(x,y))
+        if x==1000:
+            x=0
+    if change==2:
+        screen.fill(black)
+        x=x-10
+        img=pygame.image.load(runcatr[a])
+        time.sleep(0.05)
+        a=a+1
+        if a==8:
+            a=0
+        screen.blit(img,(x,y))
+        if x==0:
+            x=1000
+    pygame.display.update()
+    
