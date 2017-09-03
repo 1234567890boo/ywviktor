00bcdd25b3976652bcbe6a519d36ed4be24239f5
suraj Rajan <surajrajan0109@gmail.com>
2017-09-03 18:57:40 +0000

    weee

---
+++ class.py
@@ -0,0 +1,9 @@
+class zoo:
+    def __init__(self):
+        print("Hi this is")
+zoo1=zoo()
+class zoo2:
+    def __init__(self):
+        self.zooname="california zoo"
+zoo2=zoo2()
+print(zoo2.zooname)
