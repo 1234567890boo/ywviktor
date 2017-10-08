a0b366bf9a62a15416d41898315737e0b75b81ed
Lalithram <lalithram.tirumalasetty@gmail.com>
2017-10-08 12:10:40 -0700

    weee

---
+++ homework.py
@@ -0,0 +1,75 @@
+'''print('Viktor Gonodanov-Meydbray')
+print('718')
+print('Old San fransico')
+print('road')
+print('california')
+print('U.S.A')
+
+
+print('what is your favotite place?')
+place=input()
+print(place,' is a great place.I also want to go to ',place,'.') 
+
+
+
+print('what is your name?')
+name=input()
+print('what is yur age?')
+age=input()
+print('what is your phone number?')
+phone=input()
+print('Great to know you',name,'.You seem very smart for a',age,'year oldThanks for sharing your phone number',phone,'.Will stay in touch.')
+
+
+print('Approximatley how many students are in each grade?')
+g=int(input())
+print('how many grades are going?')
+gg=int(input())
+print('what is the capacity for each bus?')
+c=int(input())
+print('a total of',g*gg,'are going.',(g*gg)/c,'buses are neded.')
+
+
+print('what is your name?')
+n=input()
+print('do you like music?')
+m=input()
+if m=='yes':
+    print('what kind of instument do you like?')
+    i=input()
+    print('name:',n,'likes music:yes instrument prefrince:',i,)
+if m=='no':
+    print('What do you like to do?')
+    l=input()
+    print('name:',n,'likes to:',l,)
+
+
+print('what is your name?')
+n=input()
+print('do you like soccer or football?')
+l=input()
+if l=='soccer':
+    print('do you like Leonel messi?')
+    li=input()
+    print('name:',n,'likes soccer likes Leonel messi:',li,)
+if l=='football':
+    print('do you like tom brady?')
+    li=input()
+    print('name:',n,'likes football: likes tom brady:',li,)
+
+
+cc=5
+fm=17
+print(fm-cc,'family memebers will have to take a bus.')'''
+
+
+print('what stste do you live in?')
+s=input()
+if s=='california':
+    print('california has great weather.')
+elif s=='arizona':
+    print('arizona must be hot')
+elif s=='washington':
+    print('washington gets a lot of rain.')
+else:
+    print('thank you for teaching me that florida has stormy weather.')
