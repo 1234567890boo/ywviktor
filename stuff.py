097ba02fd8c0904c562c9e82ae0ab9b23a512796
Saif Habib <saroshsalim3@gmail.com>
2017-12-03 19:58:56 +0000

    weee

--- stuff.py
+++ stuff.py
@@ -1,18 +1,21 @@
 from tkinter import*
 master=Tk()
-master.title("UR MOM!!!!")
-##l1=Label(master,text="First name").grid(row=0)
-##b1=Button(master,text="UR MOM").grid(row=1,column=0)
-##e1=Entry(master)
-##e1.grid(row=0,column=1)
-##def print_text():
-##    print("Name: %s"% e1.get())
-##print_text()
-##e1.insert(END,"Enter your name here")
-##e1.delete(0,END)
+master.title("stuff")
+'''l1=Label(master,text="First Name").grid(row=0)#label
+b1=Button(master,text="click to close",command=master.quit).grid(row=1,column=0)#button
+e1=Entry(master)#entry
+e1.grid(row=0,column=1)#entry'''
+l1=Label(master,text="First Name").grid(row=0)
+l1=Label(master,text="Last Name").grid(row=1)
+l1=Label(master,text="Address").grid(row=2)
+l1=Label(master,text="Phone Number").grid(row=3)
 e1=Entry(master)
-e1=Label(master,text="First Name").grid(row=0)
-e1=Label(master,text="Last Name").grid(row=1)
-e1=Label(master,text="Address").grid(row=2)
-e1=Label(master,text="Phone Number").grid(row=3)
+e2=Entry(master)
+e3=Entry(master)
+e4=Entry(master)
+e1.grid(row=0,column=1)
+e2.grid(row=1,column=1)
+e3.grid(row=2,column=1)
+e4.grid(row=3,column=1)
+
 mainloop()
