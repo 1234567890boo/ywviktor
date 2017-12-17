9f5549118825060beb7b1eefefd285eeb4cd6b7d
Your Name <you@example.com>
2017-12-17 19:03:05 +0000

    weee

--- stuff2.py
+++ stuff2.py
@@ -5,16 +5,22 @@ master.title("temp convert")
 b1=Button(master,text="click to close",command=master.quit).grid(row=1,column=0)#button
 e1=Entry(master)#entry
 e1.grid(row=0,column=1)#entry'''
-
+num=StringVar()
 def ctf():
-    c=num.get()
+    c=e1.get()
     c=float(c)
     f=c*9/5+32
-    print(f)
+    num.set(f)
+def ftc():
+    f=e1.get()
+    f=float(f)
+    c=f/9*5-32
+    num.set(c)
+l1=Label(master,textvariable=num).grid(row=1)
 e1=Entry(master)
-e1.pack()
-num=IntVar()
+e1.grid(row=0,column=0)
 b1=Button(master,text="celcius to fahrenheit",command=ctf)
-b1.pack()
-b1=Button(master,text="fahrenheit to celcius")
+b1.grid(row=0,column=1)
+b2=Button(master,text="fahrenheit to celcius",command=ftc)
+b2.grid(row=1,column=1)
 mainloop()
