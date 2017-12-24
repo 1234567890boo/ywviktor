30a23f67209e3e9bef764e0b3f63b6b9a3b82346
Your Name <you@example.com>
2017-12-24 20:11:51 +0000

    weee

---
+++ stuff5.py
@@ -0,0 +1,9 @@
+from tkinter import*
+master=Tk()
+master.title("IDK what to call this")
+'''l1=Label(master,text="First Name").grid(row=0)#label
+b1=Button(master,text="click to close",command=master.quit).grid(row=1,column=0)#button
+e1=Entry(master)#entry
+e1.grid(row=0,column=1)#entry'''
+logo=PhotoImage(file="shrek2.gif")
+w1=Label(master,image=logo).grid(row=0,column=0)
