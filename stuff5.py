698a7e565bb06ecb89f71e3f27af5695ab53ec39
Your Name <you@example.com>
2017-12-25 00:35:20 +0000

    weee

--- stuff5.py
+++ stuff5.py
@@ -5,5 +5,11 @@ master.title("IDK what to call this")
 b1=Button(master,text="click to close",command=master.quit).grid(row=1,column=0)#button
 e1=Entry(master)#entry
 e1.grid(row=0,column=1)#entry'''
-logo=PhotoImage(file="shrek2.gif")
-w1=Label(master,image=logo).grid(row=0,column=0)
+def ShowCoice():
+    print(v.get())
+l1=Label(master,text="The Nerd Test!").pack(anchor=W)
+v=IntVar()
+v.set(1)
+stuff=[("pie",1),("pi",2)]
+for txt,val in stuff:
+    Radiobutton(master,text=txt,padx=20,variable=v,command=ShowCoice,value=val).pack(anchor=W)
