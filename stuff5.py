62266a818feea3d81c615c88f91de262cd1ffa42
Your Name <you@example.com>
2017-12-31 11:26:16 -0800

    weee

--- stuff5.py
+++ stuff5.py
@@ -6,10 +6,13 @@ b1=Button(master,text="click to close",command=master.quit).grid(row=1,column=0)
 e1=Entry(master)#entry
 e1.grid(row=0,column=1)#entry'''
 def ShowCoice():
-    print(v.get())
-l1=Label(master,text="The Nerd Test!").pack(anchor=W)
-v=IntVar()
-v.set(1)
-stuff=[("pie",1),("pi",2)]
+    g=v.get()
+    v.set(g)
+    l2=Label(master,textvariable=v).grid(row=6,sticky=W)
+a=1
+l1=Label(master,text="What's your Favorte Color?").grid(row=0,sticky=W)
+v=StringVar()
+stuff=[("Blue",'Correct'),("Green",'Incorrect'),("Red",'Wrong'),("Orange",'Not Right'),("Yellow",'Not Correct')]
 for txt,val in stuff:
-    Radiobutton(master,text=txt,padx=20,variable=v,command=ShowCoice,value=val).pack(anchor=W)
+    Radiobutton(master,text=txt,padx=20,variable=v,command=ShowCoice,value=val).grid(row=a,column=0)
+    a=a+1
