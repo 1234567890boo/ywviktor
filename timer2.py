d1411877a98365d2480aa21c12447132f95f94d2
Your Name <you@example.com>
2018-01-14 11:51:37 -0800

    weee

--- timer2.py
+++ timer2.py
@@ -6,10 +6,8 @@ master.title("Timer")
 b1=Button(master,text="click to close",command=master.quit).grid(row=1,column=0)#button
 e1=Entry(master)#entry
 e1.grid(row=0,column=1)#entry'''
-SSS=0
-MMM=0
-HHH=0
-def start():
+textt=StringVar()
+def update_time():
     global SSS,MMM,HHH, state
     if state:
         SSS=SSS+1
@@ -19,21 +17,32 @@ def start():
         if MMM>=60:
             HHH=HHH+1
             MMM=0
-        show='{0:02d}:{1:02d}:{3:02d}'.format(HHH,MMM,SSS)
-        timetext.configure(text=show)
-        master.update()
+        t=p.format(HHH,MMM,SSS)
+        textt.set(t)
+    master.after(1,update_time)
 def start1():
+    global state
     state=True
+    update_time()
 def reset():
     global state,SSS,MMM,HHH
     SSS=0
     MMM=0
     HHH=0
     state=False
+    t=p.format(HHH,MMM,SSS)
+    textt.set(t)
 def pause():
     global state
     state=False
-b1=Button(master,text="start",command=start1).grid(row=1,column=0)
-b1=Button(master,text="pause",command=pause).grid(row=1,column=2)
-b1=Button(master,text="reset",command=reset).grid(row=1,column=3)
-l1.Label(master,text=
+p='{0:02d}:{1:02d}:{2:02d}'
+SSS=0
+MMM=0
+HHH=0
+state=False
+timetext=Label(master,textvariable=textt,font=('None',25)).pack()
+b1=Button(master,text="start",command=start1).pack()
+b1=Button(master,text="pause",command=pause).pack()
+b1=Button(master,text="reset",command=reset).pack()
+update_time()
+mainloop()
