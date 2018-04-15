cd38aedf99d7f817c2e89ff65ed0be25fd12ad88
Your Name <you@example.com>
2018-04-15 18:55:53 +0000

    weee

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3
-conn=sqlite3.connect('sample.db')
+conn=sqlite3.connect('sample3.db')
 c=conn.cursor()
-'''
+
 c.execute('Create table users(id INTEGER PRIMARY KEY, name TEXT, phone TEXT, email TEXT unique, password TEXT)')
 name1='Vishal'
 phone1='855-987-2342'
@@ -22,11 +22,20 @@ print('users inserted')
 c.execute('SELECT name, email, phone FROM users')
 rows=c.fetchall()
 for r in rows:
-     print(r);'''
+     print(r)
 
+     
 newphone='431-421-5435'
 userid=1
-c.execute('UPDATE users SET phone=? WHERE id=?',(phone,id))
-
+c.execute('UPDATE users SET phone=? WHERE id=?',(newphone,userid))
+try:
+    conn=sqlite3.connect('example.db')
+    c=conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS \
+                        users(id INTEGER PRIMARY KEY, name TEXT, phone TEXT, email TEXT unique, password TEXT)')
+    conn.commit()
+except Exception as e:
+    conn.rollback()
+    print('rollback')
 conn.commit()
 c.close()
