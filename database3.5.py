3db981418bcb7c130e6418364fa930e0e0b5a1e2
Your Name <you@example.com>
2018-05-27 22:01:36 +0000

    weee

--- database3.5.py
+++ database3.5.py
@@ -1,7 +1,10 @@
 import sqlite3
 conn=sqlite3.connect('database101.db')
 c=conn.cursor()
-c.execute('SELECT * FROM Students WHERE StudentName LIKE "";')
+c.execute('SELECT COUNT(StudentID),Country FROM students GROUP BY Country ORDER BY Country;')
+c.execute('SELECT COUNT(StudentID),Country FROM students GROUP BY Country ORDER BY StudentID;')
+
+
 conn.commit()
 for record in c.fetchall():
     print(record)
---
+++ minecraft.py
@@ -0,0 +1,5 @@
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+tnt=46
+mc.setBlock(x+1,y+1,z+1,tnt,1)
+
