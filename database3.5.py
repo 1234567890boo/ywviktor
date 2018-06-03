f6f4ef07e54b8fd0af7e08ab0eef3bae3b17dd0c
Your Name <you@example.com>
2018-06-03 21:03:11 +0000

    weee

--- database3.5.py
+++ database3.5.py
@@ -1,9 +1,13 @@
 import sqlite3
 conn=sqlite3.connect('database101.db')
 c=conn.cursor()
-c.execute('SELECT COUNT(StudentID),Country FROM students GROUP BY Country ORDER BY Country;')
-c.execute('SELECT COUNT(StudentID),Country FROM students GROUP BY Country ORDER BY StudentID;')
-
+c.execute('SELECT COUNT(studentId) FROM Classes GROUP BY StudentId HAVING StudentId >10 ORDER BY StudentId')
+#Inner Join:SELECT column_name(s) FROM table1 INNER JOIN table2 ON table1.column_name=table2.column;
+#Left Join:SELECT column_name(s) FROM table1 LEFT JOIN table2 ON table1.column_name=table2.column;
+#Self Join:SELECT column_name(s) FROM table1 T1, table2 T2 WHERE condition;
+#Union SQL:SELECT column_name(s) FROM table1 UNION SELECT column_name(s) table2;
+#Group Statement:SELECT column_name(s) FROM table_name WHERE condition GROUP BY column_name(s) ORDER BY column_name(s);
+#Having Statement:SELECT column_name(s) FROM table_name WHERE condition GROUP BY column_name(s) HAVING condition ORDER BY column_name(s);
 
 conn.commit()
 for record in c.fetchall():
