2a907a2f7af6f5fba4d55015ccaa2720b1ad793c
Your Name <you@example.com>
2018-06-03 22:02:30 +0000

    weee

--- multithread.py
+++ multithread.py
@@ -1,8 +1,10 @@
 import threading
 def print_cube(num):
-    print("Cube:{}".format(num*num*num))
+    while 1:
+        print("Cube:{}".format(num*num*num))
 def print_square(num):
-    print("Square:{}".format(num*num))
+    while 1:
+        print("Square:{}".format(num*num))
 if __name__=="__main__":
     t1=threading.Thread(target=print_square,args=(10,))
     t2=threading.Thread(target=print_cube,args=(10,))
