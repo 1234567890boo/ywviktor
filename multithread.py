757ac1b1be8a52881be9b514d343d07a9ab40a28
Your Name <you@example.com>
2018-06-17 18:59:49 +0000

    weee

--- multithread.py
+++ multithread.py
@@ -1,10 +1,15 @@
 import threading
+import time
 def print_cube(num):
     while 1:
+        time.sleep(0.25)
         print("Cube:{}".format(num*num*num))
+        print()
 def print_square(num):
     while 1:
+        time.sleep(0.25)
         print("Square:{}".format(num*num))
+        print()
 if __name__=="__main__":
     t1=threading.Thread(target=print_square,args=(10,))
     t2=threading.Thread(target=print_cube,args=(10,))
--- multithreading1.py
+++ multithreading1.py
@@ -1,14 +1,25 @@
 import threading
-def print_cube(num):
-    print("Cube:{}".format(num*num*num))
-def print_square(num):
-    print("Square:{}".format(num*num))
-if __name__=="__main__":
-    t1=threading.Thread(target=print_square,args=(10,))
-    t2=threading.Thread(target=print_cube,args=(10,))
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-    print("Done!")
-
+import time
+exitFlag=0
+class myThread(threading.Thread):
+    def __init__(self,threadID,name,counter):
+        threading.Thread.__init__(self)
+        self.threadID=threadID
+        self.name=name
+        self.counter=counter
+    def run(self):
+        print("Starting"+self.name)
+        print_time(self.name,5,self.counter)
+        print("Exiting"+self.name)
+def print_time(threadName,counter,delay):
+    while counter:
+        if exitFlag:
+            threadName.exit()
+        time.sleep(delay)
+        print("%s: %s:"%(threadName,time.ctime(time.time)))
+        counter-=1
+thread1=myThread(1,"Thread-1",1)
+thread2=myThread(2,"Thread-2",2)
+thread1.start()
+thread2.start()
+print("Exiting Main Thread")              
