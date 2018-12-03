4eb3d474a295d38054aa4ed18e1f5cdf15caffb7
Your Name <you@example.com>
2018-12-02 18:16:47 -0800

    weee

---
+++ test.py
@@ -0,0 +1,7 @@
+x=1
+while x==(1,2,3,4,5,6,7,8,9,10):
+    for n in range(1,11):
+        for s in range(1,11):
+            print(s, end='')
+        print()
+        x=x+1
---
+++ tezst.py
@@ -0,0 +1,12 @@
+'''for n in range(1,11):
+    for s in range(1,11):
+        print(s, end='')
+    print()'''
+
+s=1
+while True:
+    print(s, end='')
+    s=s+1
+    if s==11:
+         s=1
+    print()
