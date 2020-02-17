7fe246816de8cb1969628ea54edc2fc25946d773
1234567890boo <viktor.meydbray19@mydiscoveryk8.org>
2020-02-16 20:18:24 -0800

    weee

--- Quiz(TK).py
+++ Quiz(TK).py
@@ -1,23 +1,73 @@
 import tkinter
 window=tkinter.Tk()
-window.title('Game Start')
+window.title('Thw Quiz Of The Wrong')
 f1=tkinter.Frame(window)
 f2=tkinter.Frame(window)
 f3=tkinter.Frame(window)
-f4=tkinter.Frame(window)
+f5=tkinter.Frame(window)
+f6=tkinter.Frame(window)
+f7=tkinter.Frame(window)
+f8=tkinter.Frame(window)
+
+points=0
 
 def yes():
     f1.grid_forget()
     f2.grid()
+    global points
+    points=1
+
 def no():
     f1.grid_forget()
     f3.grid()
+
 def two():
     f2.grid_forget()
-    f4.grid()
-def eleven():
-    pass
+    f5.grid()
 
+def eleven():
+    f2.grid_forget()
+    f5.grid()
+    global points
+    points+=1
+
+def nineteen():
+    f5.grid_forget()
+    f6.grid()
+
+def twentyone():
+    f5.grid_forget()
+    f6.grid()
+    global points
+    points+=1
+
+def dumb():
+    f6.grid_forget()
+    f7.grid()
+    global points
+    points+=1
+
+def nice():
+    f6.grid_forget()
+    f7.grid()
+
+def baygull():
+    f7.grid_forget()
+    f8.grid()
+    global points
+    points+=1
+    l9['text']='you had '+str(points)+' points'
+
+def muffin():
+    f7.grid_forget()
+    f8.grid()
+    l9['text']='you had '+str(points)+' points'
+
+def restart():
+    f8.grid_forget()
+    f1.grid()
+
+    
 #frame 1
 l=tkinter.Label(f1,text='Do you want to take a quiz')
 b=tkinter.Button(f1,text='Yes',command=yes)
@@ -36,13 +86,39 @@ b3.grid(row=1,column=0)
 b4.grid(row=1,column=1)
 
 #frame no
-l3=tkinter.Label(f3,text='THEN GO AWAY!')
+l3=tkinter.Label(f3,text='THEN GO WAY')
 b5=tkinter.Button(f3,text='exit',command=exit)
 l3.grid(row=0,column=0)
 b5.grid(row=1,column=0)
 
-# frame 2
-l4=tkinter.Label(f4,text='WRONG!!')
-b6=tkinter.Button(f4,text='exit',command=exit)
-l4.grid(row=0,column=0)
-b6.grid(row=1,column=0)
+# frame eleven
+l5=tkinter.Label(f5,text='what is 9+10?')
+b7=tkinter.Button(f5,text='19',command=nineteen)
+b8=tkinter.Button(f5,text='21',command=twentyone)
+l5.grid(row=0,column=0,columnspan=2)
+b7.grid(row=1,column=0)
+b8.grid(row=1,column=1)
+
+#frame twentyone
+l6=tkinter.Label(f6,text='what is the square of 13?')
+b9=tkinter.Button(f6,text='Im to dumb to know',command=dumb)
+b10=tkinter.Button(f6,text='169',command=nice)
+l6.grid(row=0,column=0,columnspan=2)
+b9.grid(row=1,column=0)
+b10.grid(row=1,column=1)
+
+#frame dumb
+l7=tkinter.Label(f7,text='why dont seagulls fly over the bay')
+b11=tkinter.Button(f7,text='becuse baygulls',command=baygull)
+b12=tkinter.Button(f7,text='because muffin',command=muffin)
+l7.grid(row=0,column=0,columnspan=2)
+b12.grid(row=1,column=1)
+b11.grid(row=1,column=0)
+
+#frame Baygull
+l8=tkinter.Label(f8,text='YAY, YOU WON!! YOU WON BRAGGING WRIGHTS!!')
+l9=tkinter.Label(f8,text='you had '+str(points)+' points')
+b13=tkinter.Button(f8,text='Do You Want To Restart?',command=restart)
+l8.grid(row=0,column=0)
+l9.grid(row=1,column=0)
+b13.grid(row=2,column=0)
