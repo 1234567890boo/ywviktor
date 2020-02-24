521cf45ae036411ae0bdcf36e5d27d5b55c32f44
1234567890boo <viktor.meydbray19@mydiscoveryk8.org>
2020-02-23 16:50:49 -0800

    weee

--- OOP.py
+++ OOP.py
@@ -1,46 +1,27 @@
-'''class Bike:
-    def __init__(self,color,num_brakes,num_wheels,sw,frames,size):
-        self.color=color
-        self.num_brakes=num_brakes
-        self.num_wheels=num_wheels
-        self.sw=sw
-        self.frames=frames
-        self.size=size
-    def detailed_print(self):
-        print(self.color)
-        print(self.num_brakes)
-        print(self.num_wheels)
-        print(self.sw)
-        print(self.frames)
-        print(self.size)'''
+import pygame,time,random
 
-class Bike:
-    def __init__(self):
-        self.color='red'
-        self.num_brakes='2'
-        self.num_wheels='2'
-        self.sw='1'
-        self.frames='1'
-        self.size='big'
-        self.lock_num='12345'
-    def detailed_print(self):
-        print(self.color)
-        print(self.num_brakes)
-        print(self.num_wheels)
-        print(self.sw)
-        print(self.frames)
-        print(self.size)
-        print(self.lock_num)
-    def change_lock(self):
-        
-        print('what is the lock password?')
-        lock=input()
-        if lock!=self.lock_num:
-            print('you are not authorised to change the lock number')
-        else:
-            print('what is the new lock number?')
-            self.lock_num=input()
-            print('lock number is now '+self.lock_num)
-bike=Bike()
-#bike2=Bike('blue','0 brakes','2 wheels','2 steering wheels','1 frame','small')
-#bike.detailed_print()
+pygame.init()
+
+width=500
+height=500
+
+screen=pygame.display.set_mode((width,height))
+
+pygame.display.set_caption("Snake OOP")
+
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+white=(255,255,255)
+black=(0,0,0)
+
+class Food:
+    def __init__(self,x,y,width,height):
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+    def draw(self):
+        pygame.draw.rect(screen,green,(self.x,self.y,self.width,self.height))
+food=Food(random.randint(30,470),random.randint(30,470),10,10)
+food.draw()
