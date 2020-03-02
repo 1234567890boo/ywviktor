433d7b0b09139bc93bf04a420089767bd719f35b
1234567890boo <viktor.meydbray19@mydiscoveryk8.org>
2020-03-01 17:19:14 -0800

    weee

--- OOP.py
+++ OOP.py
@@ -3,8 +3,8 @@ from pygame.locals import*
 
 pygame.init()
 
-width=500
-height=500
+width=1000
+height=1000
 
 screen=pygame.display.set_mode((width,height))
 
@@ -16,58 +16,78 @@ blue=(0,0,255)
 white=(255,255,255)
 black=(0,0,0)
 
+points=0
+clock=pygame.time.Clock()
+
+def show_text(msg,x,y,color):
+  fontobj=pygame.font.SysFont("freesans",32)
+  msgobj=fontobj.render(msg,False,color)
+  screen.blit(msgobj,(x,y))
+
 class Food:
     def __init__(self):
-        self.x=random.randint(30,470)
-        self.y=random.randint(30,470)
-        self.width=10
-        self.height=10
+        self.x=(random.randint(30,970)//20)*20
+        self.y=(random.randint(30,970)//20)*20
+        self.width=20
+        self.height=20
     def draw(self):
         pygame.draw.rect(screen,red,(self.x,self.y,self.width,self.height))
 
 class Snake:
     def __init__(self):
-        self.x=250
-        self.y=250
-        self.width=10
-        self.height=10
-        self.vel=0.25
+        self.x=500
+        self.y=500
+        self.width=20
+        self.height=20
+        self.vel=20
         self.up=False
         self.down=False
         self.left=False
         self.right=False
     def draw(self):
         pygame.draw.rect(screen,green,(self.x,self.y,self.width,self.height))
-
+        
+def grid():
+    x=20
+    y=20
+    for n in range(0,101,1):
+        pygame.draw.line(screen,white,(x,0),(x,1000),1)
+        pygame.draw.line(screen,white,(0,y),(1000,y),1)
+        x+=20
+        y+=20
+    
 snake=Snake()
 food=Food()
 
 while True:
     screen.fill(black)
-    snake.draw()
+    show_text('points='+str(points),20,900,red)
     food.draw()
+    grid()
+    snake.draw()
+    clock.tick(10)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type==QUIT:
             pygame.quit()
             exit()
         if event.type==KEYDOWN:
-            if event.key==K_w:
+            if event.key==K_w and snake.down==False:
                 snake.up=True
                 snake.down=False
                 snake.left=False
                 snake.right=False
-            elif event.key==K_s:
+            elif event.key==K_s and snake.up==False:
                 snake.down=True
                 snake.up=False
                 snake.left=False
                 snake.right=False
-            elif event.key==K_a:
+            elif event.key==K_a and snake.left==False:
                 snake.right=True
                 snake.left=False
                 snake.up=False
                 snake.down=False
-            elif event.key==K_d:
+            elif event.key==K_d and snake.right==False:
                 snake.left=True
                 snake.right=False
                 snake.up=False
@@ -80,6 +100,11 @@ while True:
         snake.x+=snake.vel
     elif snake.right==True:
         snake.x-=snake.vel
+    if snake.x==food.x and snake.y==food.y:
+        food.x=(random.randint(30,970)//20)*20
+        food.y=(random.randint(30,970)//20)*20
+        points+=1
+    
 #Homework: make left and right movement and be able to eat food
 
 
