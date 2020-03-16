c11e2a982ee5f1f6aa4eb6ebf05a305a675953c5
1234567890boo <viktor.meydbray19@mydiscoveryk8.org>
2020-03-15 17:25:04 -0700

    weee

---
+++ OOPsnakeMenu.py
@@ -0,0 +1,41 @@
+import pygame,random,time,OOP
+from pygame.locals import*
+
+pygame.init()
+
+width=1000
+height=900
+
+screen=pygame.display.set_mode((width,height))
+
+pygame.display.set_caption("Snake menu")
+
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+white=(255,255,255)
+black=(0,0,0)
+
+points=0
+clock=pygame.time.Clock()
+
+def show_text(msg,x,y,color):
+  fontobj=pygame.font.SysFont("freesans",100)
+  msgobj=fontobj.render(msg,False,color)
+  screen.blit(msgobj,(x,y))
+
+while True:
+    screen.fill(black)
+    show_text('One player',20,20,green)
+    show_text('Two players',550,20,green)
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            exit()
+        if event.type==MOUSEBUTTONDOWN and event.button==1:
+            if event.pos[0]>=20 and event.pos[0]<=385 and event.pos[1]>=20 and event.pos[1]<=100:
+                print('oneplayer')
+            if event.pos[0]>=550 and event.pos[0]<=850 and event.pos[1]>=20 and event.pos[1]<=100:
+                OOP.player2()
+    pygame.display.update()
+    
