8c2102a55e8b2bc53eb8c7b56657c07f0ed34ff0
Misha Gonodanov <mgonodanov@conviva.com>
2020-03-23 15:10:05 -0700

    weee

--- maze_game.py
+++ maze_game.py
@@ -45,30 +45,41 @@ class Wall:
     def draw(self, x , y):
         pygame.draw.rect(screen,black,(x*self.height, y*self.width, self.width, self.height))
 
+class Foe:
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+        self.vel=10
+    def draw(self,x,y):
+        if world[x][y]==f:
+            pygame.draw.rect(screen,red,(x*self.height, y*self.width, self.width, self.height))
+
 w=Wall(10,10)
 e=Empty(10,10)
-            
+f=Foe(10,10)
+
 world=[[w,w,w,w,w,w,w,w,w,w],
       [w,e,e,e,e,e,e,e,e,w],
-      [w,e,e,e,e,e,e,e,e,w],
+      [w,e,e,w,w,w,e,w,e,w],
+      [w,w,e,w,e,e,e,w,e,w],
+      [w,e,e,e,e,w,w,w,e,w],
+      [w,e,e,w,w,w,e,w,e,w],
       [w,e,e,e,e,e,e,w,e,w],
-      [w,e,e,e,e,e,e,e,e,w],
-      [w,e,e,e,e,e,e,e,e,w],
-      [w,e,e,e,e,e,e,e,e,w],
-      [w,e,e,e,e,e,e,e,e,w],
-      [w,e,e,e,e,e,e,e,e,w],
+      [w,e,e,w,w,w,e,e,e,w],
+      [w,e,e,e,e,e,e,e,f,w],
       [w,w,w,w,w,w,w,w,w,w]]
 
+player=Player(10,10)
+px=2
+py=2
+world[px][py]=player
 def drawMap():
     screen.fill(white)
     for x in range(0,10,1):
             for y in range(0,10,1):
                 world[x][y].draw(x,y)
 
-player=Player(10,10)
-px=2
-py=2
-world[px][py]=player
+
 
 def MovePlayer(shift_x, shift_y):
     global px
@@ -87,12 +98,12 @@ while True:
             pygame.quit()
             exit()
         if event.type==KEYDOWN:
-            if event.key==K_w:
+            if event.key==K_w or event.key==K_UP:
                 MovePlayer(0,-1)
-            if event.key==K_s:
+            if event.key==K_s or event.key==K_DOWN:
                 MovePlayer(0,1)
-            if event.key==K_a:
+            if event.key==K_a or event.key==K_LEFT:
                 MovePlayer(-1,0)
-            if event.key==K_d:
+            if event.key==K_d or event.key==K_RIGHT:
                 MovePlayer(1,0)
     pygame.display.update()
