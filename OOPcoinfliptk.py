35b18eafda1e5038c936d9e887c675cbe2725c71
Misha Gonodanov <mgonodanov@conviva.com>
2020-04-12 19:13:52 -0700

    weee

--- OOPcoinfliptk.py
+++ OOPcoinfliptk.py
@@ -1,38 +1,51 @@
 from tkinter import *
+import json
+
+##fruits={'apple':{'price':1.32,'row':0,'column':0},
+##          'banana':{'price':0.24,'row':0,'column':1},
+##          'orange':{'price':2.5,'row':0,'column':2},
+##          'mango':{'price':3.00,'row':1,'column':0},
+##          'grape':{'price':0.05,'row':1,'column':1},
+##          'peach':{'price':1.63,'row':1,'column':2}}
+
+
+##fruits_json = json.dumps(fruits, sort_keys=True, indent=2)
+##f = open("fruits.json","w")
+##f.write(fruits_json)
+##f.close()
+
+##vim fruits.json to read/wright json file
+##:x to save and close
+
+with open('fruits.json') as f:
+  fruits=json.load(f)
 
 class Fruit:
-    def __init__(self):
+    def __init__(self,fruits):
         self.window=Tk()
         self.select=StringVar()
         self.select.set('apple')
-        self.1=Radiobutton(self.window,text='apple',variable=self.select,value='apple')
-        self.2=Radiobutton(self.window,text='banana',variable=self.select,value='banana')
-        self.3=Radiobutton(self.window,text='orange',variable=self.select,value='orange')
-        self.4=Radiobutton(self.window,text='mango',variable=self.select,value='mango')
-        self.5=Radiobutton(self.window,text='grape',variable=self.select,value='grape')
-        self.6=Radiobutton(self.window,text='peach',variable=self.select,value='peach')
+        self.fruits=fruits
+        self.butt={}
+        for n in self.fruits.keys():
+            self.butt[n]=Radiobutton(self.window,text=n,variable=self.select,value=n)
         self.l=Label(self.window,text='Enter the quantitiy of fruit you want.')
         self.l2=Label(self.window,text='you have to pay $(selected fruit*num)')
         self.e=Entry(self.window)
         self.b=Button(self.window,text='Pay',command=self.findprice)
     def draw(self):
-        self.apple.grid(row=0,column=0)
-        self.banana.grid(row=0,column=1)
-        self.orange.grid(row=0,column=2)
-        self.mango.grid(row=1,column=0)
-        self.grape.grid(row=1,column=1)
-        self.peach.grid(row=1,column=2)
-        self.l.grid(row=2,column=0,columnspan=3)
-        self.e.grid(row=3,column=0,columnspan=3)
-        self.l2.grid(row=4,column=0,columnspan=3)
-        self.b.grid(row=5,column=0,columnspan=3)
+        for s in self.fruits.keys():
+            self.butt[s].grid(row=self.fruits[s]['row'],column=self.fruits[s]['column'])
+        self.l.grid(row=3,column=0,columnspan=3)
+        self.e.grid(row=4,column=0,columnspan=3)
+        self.l2.grid(row=5,column=0,columnspan=3)
+        self.b.grid(row=6,column=0,columnspan=3)
     def findprice(self):
         num=int(self.e.get())
         fruit=self.select.get()
-        fprice={'apple':1.32,'banana':0.24,'orange':2.5,'mango':3.00,'grape':0.05,'peach':1.63}
-        self.l2['text']='you have to pay $'+str(fprice[fruit]*num)+' for '+str(self.select.get())
-        
-w=Fruit()
+        self.l2['text']='you have to pay $'+str(self.fruits[fruit]['price']*num)+' for '+str(self.select.get())
+
+w=Fruit(fruits)
 w.draw()
 w.window.mainloop()
 #Homework:give class dictionary with fruits and prices and display it on screen
