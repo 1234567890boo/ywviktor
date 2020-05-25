5ecbe9413b3de0fad000f3a926e565d8ccb7f0e7
1234567890boo <viktor.meydbray19@mydiscoveryk8.org>
2020-05-24 17:18:10 -0700

    weee

--- databaseui.py
+++ databaseui.py
@@ -4,15 +4,6 @@ connect=sqlite3.connect('new.db')
 cursor=connect.cursor()
 table='create TABLE if not exists '
 
-print('what do you want to do?')
-print('1=make table')
-print('2=insert into table')
-print('3=delete from table')
-print('4=update table entries')
-print('5=look at data')
-
-r=int(input())
-
 def maketable():
     pk=False
     print('what is the table name?')
@@ -60,7 +51,25 @@ def insertinto():
         insert=insert+')'
         print(insert)
         cursor.execute(insert)
-#menu based program
+        
+def read_data():
+    n=1
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    fetch=cursor.fetchall()
+    print('what table do you want to see?')
+    for t in fetch:
+                   print(n,'=',t[0])
+                   n+=1
+    i=int(input())
+    if i<len(fetch):
+        cursor.execute("SELECT * FROM {}".format(fetch[i-1][0]))
+        f=cursor.fetchall()
+        for c in cursor.description:
+            print(c[0],end=' ')
+        print('')
+        for s in range(0,len(f),1):
+            print(f[s])
+
 print('what do you want to do?')
 print('1=make table')
 print('2=insert into table')
@@ -70,10 +79,28 @@ print('5=look at data')
 
 r=int(input())
 
-if r==5:
-    print('what table do you want to see?')
+if r==3:
+    n=1
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-    print(cursor.fetchall())
+    fetchtable=cursor.fetchall()
+    print('what table do you want to delete from?')
+    for t in fetchtable:
+                   print(n,'=',t[0])
+                   n+=1
+    i=int(input())
+    if i<len(fetchtable):
+        f=1
+        cursor.execute("SELECT * FROM {}".format(fetchtable[i-1][0]))
+        fetchvalue=cursor.fetchall()
+        print('what do you want to delete?')
+        for s in fetchvalue:
+            print(f,'=',s)
+            f+=1
+        p=int(input())
+        if p>f:
+            print('That is not in the table. Plese try again.')
+        else:
+            print('DELETE FROM {} WHERE id in (SELECT id FROM {} LIMIT 1 OFFSET 1)'.format(fetchtable[i-1][0],fetchtable[i-1][0]))
 connect.commit()
 connect.close()
 
