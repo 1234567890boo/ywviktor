dd9f047c8469fdee67740a28db28a328505b2b12
viktor <vgmeydbray@gmail.com>
2020-06-28 17:21:25 -0700

    weee

--- googleClient.py
+++ googleClient.py
@@ -1,14 +1,34 @@
 import socket
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("https://stackoverflow.com/",80))
+s.connect(("https://www.bing.com",80))
 print("Connection established")
 
-s.send("GET / HTTP/1.1\r\n\r\n".encode("utf-8"))
+s.send("GET / HTTP/1.1\r\r\r\n".encode("utf-16"))
+
+content=''
+m=4096
 
 while True:
-    msg=s.recv(4096).decode("utf-8",errors="ignore")
-    print(msg,end="")
-s.close()  
+    msg=s.recv(m).decode("utf-8",errors="ignore")
 
-#Honework: gets and prints all html code
+    print(len(msg))
+        
+    content=content+msg
+
+    if '</html>' in msg or msg<='':
+        break
+
+
+content=content.split('\n')
+
+    
+s.close()
+file=open('socketTest.html','w')
+file.write(str(content))
+file.close()
+
+
+
+
+#Honework: convert list to string and do same thing with other website
