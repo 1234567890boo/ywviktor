ade168dd81886f132e36791779b197de1842dd3e
viktor <vgmeydbray@gmail.com>
2021-01-19 17:30:46 -0800

    weee

--- LoginSystem/LoginSystemFlask.py
+++ LoginSystem/LoginSystemFlask.py
@@ -6,16 +6,30 @@ from bson.objectid import ObjectId
 
 app = Flask("Login System")
 app.config['MONGO_URI']='mongodb://127.0.0.1:27017/Jumbledata'
+app.config['SECRET_KEY']='dootdoot'
 Bootstrap(app)
 mongo=PyMongo(app)
 
 @app.route('/',methods=['GET','POST'])
 def signup():
-    return render_template('SignupPage.html')
+    if request.method=='GET':
+        return render_template('SignupPage.html')
+    else:
+        Person =request.form.to_dict()
+        mongo.db.FoodCollection.insert_one(Person)
+        message=flash('You have been regestered')
+        return render_template('SignupPage.html', Message=message)
 
 @app.route('/Loginpage',methods=['Get','Post'])
 def login():
-    return render_template('LoginPage.html')
+    if request.method == 'GET':
+        return render_template('LoginPage.html')
+    else:
+        return redirect('LoginPage')
+
+@app.route('/Realpage')
+def page():
+    return render_template('RealPage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
