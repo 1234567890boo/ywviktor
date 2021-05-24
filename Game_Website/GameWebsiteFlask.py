fdd9135941a1c175ba1017e42c32ff06b45c01f1
viktor <vgmeydbray@gmail.com>
2021-05-23 19:19:34 -0700

    finished the about me page and worked on the contact page

--- Game_Website/GameWebsiteFlask.py
+++ Game_Website/GameWebsiteFlask.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, request, redirect, flash
 from flask_pymongo import PyMongo
 
 app = Flask("Game Website")
-#app.config['MONGO_URI'] = ''
-#mongo = PyMongo(app)
+app.config['MONGO_URI'] = 'mongodb+srv://TestUser1:test@viktors-yw-database.hgfpy.mongodb.net/GameWebsite?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 @app.route('/', methods=["GET","POST"])
 def Home_Page():
@@ -13,11 +13,14 @@ def Home_Page():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET","POST"])
 def contact():
+    if request.method=='POST':
+        user=request.form.to_dict()
+        mongo.db.GameWebsiteEmails.insert_one(user)
     return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-#HW: work on the website
+#HW: get rid of empty space and add games
