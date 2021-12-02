c961c43bc45477ef0d6f066fe134f83bd76ca842
viktor <vgmeydbray@gmail.com>
2021-12-01 18:25:18 -0800

    worked on regression and fix issue with sklearn

--- MachineLearning/Regression.py
+++ MachineLearning/Regression.py
@@ -4,16 +4,35 @@ import random
 import pandas as pd
 from sklearn import linear_model
 
-df=pd.read_csv("RegressionWithCSV.csv")
+np.random.seed(2)
+x=np.random.normal(3,1,100)
+y=np.random.normal(200,50,100)/x
+
+
+train_x=x[:80]
+train_y=y[:80]
+test_x=x[80:]
+test_y=y[80:]
+
+line=np.linspace(0,6,100)
+polynomialmodel=np.poly1d(np.polyfit(train_x,train_y,4))
+
+plt.scatter(test_x,test_y)
+plt.plot(line,polynomialmodel(line))
+plt.show()
+
+'''df=pd.read_csv("RegressionWithCSV.csv")
 x=df[["Volume","Weight"]]
 y=df["CO2"]
 
 linemodel=linear_model.LinearRegression()
 linemodel.fit(x,y)
-predictedCo2=linemodel.predict([[200,150]])
+predictedCo2=linemodel.predict([[200,1]])
 print(predictedCo2)
 
-'''x=[80,43,36,36,95,10,66,34,37,20,26,29,48,64,6,5,36,66,72,42]
+#=========================================================================================================
+
+x=[80,43,36,36,95,10,66,34,37,20,26,29,48,64,6,5,36,66,72,42]
 y=[20,46,3,35,67,95,51,72,58,10,26,34,90,33,38,20,56,2,47,16]
 
 slope,intercept,r,p,std_err=stats.linregress(x,y)
